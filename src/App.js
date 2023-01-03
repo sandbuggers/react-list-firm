e09ff3cf0e452fa926b3ad2c5ba6f0d211c5f722
Abigail Bennett Lugue Carungay <abigail.carungay@leapdev.io>
2023-01-03 13:23:30 +1100

    implement filtering using built-in react-table functionality. restructure components to remove FilterableFirmTable and UserInput

--- src/App.js
+++ src/App.js
@@ -1,108 +1,191 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import firmsData from "./firms.json";
 import "./App.css";
 
 import {
-  useTable
+  useTable,
+  useFilters,
+  useGlobalFilter
 } from "react-table";
+import {matchSorter} from 'match-sorter';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>FIRMS</h1>
-      <FilterableFirmTable />
-    </div>
-  );
-}
-
-class FilterableFirmTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchValue: "",
-      states: [""],
-      status: [""],
-      flags: [""]
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <UserInput onSearchUpdate={(i) => this.setState({searchValue: i})}/>
-        <FirmTable />
-        <h3>SearchValue: {this.state.searchValue}</h3>
-      </div>
-    )
-  };
-
-}
-
-class UserInput extends React.Component {
-  render() {
-    return (
-      <div>
-        <label for="search">Search</label>
-        <input type="search" id="search-box" name="search-box" 
-        onChange={event => this.props.onSearchUpdate(event.target.value)}/>
-        <label for="states-select">States</label>
-        <select id="states-select" name="states-select" multiple>
-          <option value="">All States</option>
-          <option>NSW</option>
-          <option>SA</option>
-          <option>QLD</option>
-          <option>WA</option>
-          <option>VIC</option>
-          <option>TAS</option>
-        </select>
-        <label for="status-select">Status</label>
-        <select id="status-select" name="status-select" multiple>
-          <option value="">All Status</option>
-          <option>Enabled</option>
-          <option>Disabled</option>
-        </select>
-        <label for="flags-select">Flags</label>
-        <select id="flags-select" name="flags-select" multiple>
-          <option value="">All Flags</option>
-          <option>Test Firm</option>
-          <option>Non-Test</option>
-        </select>{" "}
-      </div>
-    );
-  }
-}
-
-
-
-
-function FirmTable() {
   const data = [...firmsData];
   const columns = React.useMemo(
     () => [
       {
         Header: "Firm Name",
-        accessor: "firmName"
+        accessor: "firmName",
+        filter: "fuzzyText"
       },
       {
         Header: "State",
-        accessor: "state"
+        accessor: "state",
+        Filter: SelectColumnFilter,
+        filter: "includes"
       },
       {
         Header: "Users",
-        accessor: "numberOfUsers"
+        accessor: "numberOfUsers",
+        Filter: NumberRangeColumnFilter,
+        filter: "between"
       },
       {
         Header: "Status",
-        accessor: "statusDescription"
+        accessor: "statusDescription",
+        Filter: SelectColumnFilter,
+        filter: "includes"
       },
       {
         Header: "Flag",
-        accessor: row => (row.isTestFirm ? "Test Firm" : "Non-Test")
+        accessor: row => (row.isTestFirm ? "Test Firm" : "Non-Test"),
+        Filter: SelectColumnFilter,
+        filter: "includes"
       }
     ], 
     []
   );
+  
+  return (
+    <div className="App">
+      <h1>FIRMS</h1>
+      <FirmTable columns={columns} data={data} />
+    </div>
+  );
+}
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id }
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set();
+    preFilteredRows.forEach((row) => {
+      options.add(row.values[id]);
+    });
+    return [...options.values()];
+  }, [id, preFilteredRows]);
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined);
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+// This is a custom UI for a 'between' or number range
+// filter. It uses two number boxes and filters rows to
+// ones that have values between the two
+function NumberRangeColumnFilter({
+  column: { filterValue = [], preFilteredRows, setFilter, id }
+}) {
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
+    preFilteredRows.forEach((row) => {
+      min = Math.min(row.values[id], min);
+      max = Math.max(row.values[id], max);
+    });
+    return [min, max];
+  }, [id, preFilteredRows]);
+
+  return (
+    <div
+      style={{
+        display: "flex"
+      }}
+    >
+      <input
+        value={filterValue[0] || ""}
+        type="number"
+        onChange={(e) => {
+          const val = e.target.value;
+          setFilter((old = []) => [
+            val ? parseInt(val, 10) : undefined,
+            old[1]
+          ]);
+        }}
+        placeholder={`Min (${min})`}
+        style={{
+          width: "70px",
+          marginRight: "0.5rem"
+        }}
+      />
+      to
+      <input
+        value={filterValue[1] || ""}
+        type="number"
+        onChange={(e) => {
+          const val = e.target.value;
+          setFilter((old = []) => [
+            old[0],
+            val ? parseInt(val, 10) : undefined
+          ]);
+        }}
+        placeholder={`Max (${max})`}
+        style={{
+          width: "70px",
+          marginLeft: "0.5rem"
+        }}
+      />
+    </div>
+  );
+}
+
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+
+function FirmTable({columns, data}) {
+  const filterTypes = React.useMemo(
+    () => ({
+      fuzzyText: fuzzyTextFilterFn
+    }),
+    []
+  )
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
 
   const {
     getTableProps,
@@ -110,7 +193,20 @@ function FirmTable() {
     headerGroups,
     rows,
     prepareRow,
-  } = useTable({ columns, data })
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn, 
+      filterTypes,
+    },
+    useFilters,
+    useGlobalFilter 
+  )
 
   return (
     <div>
@@ -119,7 +215,11 @@ function FirmTable() {
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              <th {...column.getHeaderProps()}>
+                {column.render('Header')}
+                {/* Render filter if exists*/}
+                <div>{column.canFilter ? column.render('Filter') : null}</div>
+              </th>
             ))}
           </tr>
         ))}
@@ -137,6 +237,13 @@ function FirmTable() {
         })}
       </tbody>
     </table>
+    <br/>
+    {/* show filters for debugging */}
+    <div>
+        <pre>
+          <code>{JSON.stringify(state.filters, null, 2)}</code>
+        </pre>
+      </div>
     </div>
   )
 }
